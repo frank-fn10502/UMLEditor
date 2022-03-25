478a4d597b2b28341384bd92e7f319abe09c25a5
frank-fn10502 <frank08.chen@gmail.com>
2022-03-25 14:26:22 +0000

    add ActionsServices to collect all Action type

--- src/App.java
+++ src/App.java
@@ -1,12 +1,18 @@
 import Controller.UMLEditorController;
-import Services.EditComponentsService;
+import Services.*;
+import UMLActions.*;
 import View.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
         UMLFrame frame = new UMLFrame();
-        EditComponentsService service = new EditComponentsService(frame.editArea);
+        EditComponentsService ECservice = new EditComponentsService(frame.editArea);
         
-        UMLEditorController controller = new UMLEditorController(frame, service);
+        ActionsServices actionCollection = new ActionsServices();
+        actionCollection.Select = new Select(ECservice);
+        actionCollection.CreateClass = new CreateClass(ECservice);
+        actionCollection.CreateUseCase = new CreateUseCase(ECservice);
+
+        new UMLEditorController(frame, actionCollection);
     }
 }
--- src/Controller/UMLEditorController.java
+++ src/Controller/UMLEditorController.java
@@ -1,6 +1,6 @@
 package Controller;
 
-import Services.EditComponentsService;
+import Services.*;
 import Test.Listeners.EditAreaMouseListener;
 import UMLActions.*;
 import View.UMLFrame;
@@ -9,19 +9,19 @@ import java.awt.*;
 public class UMLEditorController {
 
     private UMLFrame frame;
-    private EditComponentsService service;
+    private ActionsServices collection;
 
-    public UMLEditorController(UMLFrame frame, EditComponentsService service) {
+    public UMLEditorController(UMLFrame frame, ActionsServices collection) {
         super();
         this.frame = frame;
-        this.service = service;
+        this.collection = collection;
 
         // this.frame.editArea.addMouseListener(new EditAreaMouseListener());
         // this.frame.editArea.addMouseMotionListener(new EditAreaMouseListener());
 
         // temp
         // this.frame.editArea.addMouseListener(new CreateClass(service));
-        this.frame.editArea.addMouseListener(new CreateUseCase(service));
+        // this.frame.editArea.addMouseListener(new CreateUseCase(service));
     }
 
     // 透過 btn onclick event 更換 Listener 以達到變更 Action 的實作
