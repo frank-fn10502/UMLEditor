2d296e17d94495fa27cbea532c9c1b7e76ebc97a
frank-fn10502 <frank08.chen@gmail.com>
2022-03-26 18:51:17 +0000

    menubar add llistener

--- src/View/UMLMenuBar.java
+++ src/View/UMLMenuBar.java
@@ -1,23 +1,39 @@
 package View;
 
 import javax.swing.*;
+import java.awt.event.*;
 
 public class UMLMenuBar extends JMenuBar {
+    JMenuItem groupObj, unGroupObj, changeObjName;
+
     public UMLMenuBar() {
         super();
 
         JMenu file = new JMenu("File");
         JMenu edit = new JMenu("Edit");
         
-        JMenuItem groupObj = new JMenuItem("Group");
-        JMenuItem unGroupObj = new JMenuItem("UnGroup");
-        JMenuItem changeObjName = new JMenuItem("change object name");
-
-        edit.add(groupObj);
-        edit.add(unGroupObj);
-        edit.add(changeObjName);
+        setMenuItem(edit);
     
         this.add(file);
         this.add(edit);
+    }
+    void setMenuItem(JMenu menu){
+        groupObj = new JMenuItem("Group");
+        unGroupObj = new JMenuItem("UnGroup");
+        changeObjName = new JMenuItem("change object name");
+
+        menu.add(groupObj);
+        menu.add(unGroupObj);
+        menu.add(changeObjName);
+    }
+
+    public void setGroupListener(ActionListener listener){
+        this.groupObj.addActionListener(listener);
+    }
+    public void setUnGroupObjListener(ActionListener listener){
+        this.unGroupObj.addActionListener(listener);
+    }
+    public void setChangeObjNameListener(ActionListener listener){
+        this.changeObjName.addActionListener(listener);
     }
 }
