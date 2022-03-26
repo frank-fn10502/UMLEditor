4e5199766ef99da54237e2db251ee32c5c446d41
frank-fn10502 <frank08.chen@gmail.com>
2022-03-26 19:05:32 +0000

    refact some code

--- src/Controller/EditorAreaController.java
+++ src/Controller/EditorAreaController.java
@@ -6,21 +6,6 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class EditorAreaController {
-
-    class ListenerHelper {
-        public void addListener(Container c, MouseAdapter action) {
-            if (action == preMethod)
-                return;
-
-            c.removeMouseListener(preMethod);
-            c.removeMouseMotionListener(preMethod);
-
-            c.addMouseListener(action);
-            c.addMouseMotionListener(action);
-            preMethod = action;
-        }
-    }
-
     UMLFrame frame;
     ActionsServices collection;
     MouseAdapter preMethod;
@@ -35,39 +20,52 @@ public class EditorAreaController {
         setDefaultAction();
         bindButtonClick();
     }
-    void setDefaultAction(){
-        this.preMethod = collection.Select;
+
+    void setDefaultAction() {
+        this.preMethod = this.collection.Select;
 
         this.frame.sideBar.setCurrentClicked(this.frame.sideBar.btnSelect);
-        this.frame.editArea.addMouseListener(collection.Select);
-        this.frame.editArea.addMouseMotionListener(collection.Select);
+        this.frame.editArea.addMouseListener(this.collection.Select);
+        this.frame.editArea.addMouseMotionListener(this.collection.Select);
     }
 
     // 透過 btn onclick event 更換 Listener 以達到變更 Action 的實作
     void bindButtonClick() {
         this.frame.sideBar.btnSelect.addActionListener(e -> {
-            new ListenerHelper().addListener(this.frame.editArea, collection.Select);
+            this.addListener(this.frame.editArea, this.collection.Select);
         });
         this.frame.sideBar.btnCreateClass.addActionListener(e -> {
             service.clearSelectList();
-            new ListenerHelper().addListener(this.frame.editArea, collection.CreateClass);
+            this.addListener(this.frame.editArea, this.collection.CreateClass);
         });
         this.frame.sideBar.btnCreateUseCase.addActionListener(e -> {
             service.clearSelectList();
-            new ListenerHelper().addListener(this.frame.editArea, collection.CreateUseCase);
+            this.addListener(this.frame.editArea, this.collection.CreateUseCase);
         });
         this.frame.sideBar.btnAL.addActionListener(e -> {
             service.clearSelectList();
-            new ListenerHelper().addListener(this.frame.editArea, collection.Association);
+            this.addListener(this.frame.editArea, this.collection.Association);
         });
         this.frame.sideBar.btnGL.addActionListener(e -> {
             service.clearSelectList();
-            new ListenerHelper().addListener(this.frame.editArea, collection.Generalization);
+            this.addListener(this.frame.editArea, this.collection.Generalization);
         });
         this.frame.sideBar.btnCL.addActionListener(e -> {
             service.clearSelectList();
-            new ListenerHelper().addListener(this.frame.editArea, collection.Composition);
+            this.addListener(this.frame.editArea, this.collection.Composition);
         });
+    }
+
+    void addListener(Container c, MouseAdapter action) {
+        if (action == preMethod)
+            return;
+
+        c.removeMouseListener(preMethod);
+        c.removeMouseMotionListener(preMethod);
+
+        c.addMouseListener(action);
+        c.addMouseMotionListener(action);
+        preMethod = action;
     }
 
 }
